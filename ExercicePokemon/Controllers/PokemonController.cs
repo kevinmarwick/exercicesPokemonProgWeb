61e548d65dfedeb554e3b4f7c0a8a8a04987e717
Kevin Marwick <6107320@cegepmontpetit.ca>
2024-05-17 22:57:02 -0400

    exercices terminées
    exercices terminées

---
+++ ExercicePokemon/Controllers/PokemonController.cs
@@ -0,0 +1,30 @@
+﻿using ExercicePokemon.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExercicePokemon.Controllers
+{
+    public class PokemonController : Controller
+    {
+        public IActionResult Index()
+        {
+            var baseDeDonnees = new FausseBaseDeDonnees();
+            var donnees = baseDeDonnees.ObtenirListePokemons();
+            return View(donnees);
+        }
+
+        public IActionResult Consulter (int id)
+        {
+            var badedeDonnees = new FausseBaseDeDonnees();
+            var donnees = badedeDonnees.ObtenirListePokemons();
+            foreach(var e in donnees)
+            {
+                if(e.Id == id)
+                {
+                    return View(e);
+                }
+               
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ ExercicePokemon/Models/Pokemon.cs
@@ -0,0 +1,35 @@
+﻿namespace ExercicePokemon.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+
+        public string? Nom { get; set; }
+
+        public string? Type1 { get; set; }
+
+        public string? Type2 { get; set; }
+
+        public int Total { get; set; }
+
+        public int HP { get; set; }
+
+        public int Defense { get; set; }
+
+        public int Attaque { get; set; }
+
+        public int SpecialAttaque { get; set; }
+
+        public int SpecialDefense { get; set; }
+
+        public int Vitesse { get; set; }
+
+        public int Generation { get; set; }
+
+        public bool Legendaire { get; set; }
+
+        public string? Description { get; set; }
+
+
+    }
+}
